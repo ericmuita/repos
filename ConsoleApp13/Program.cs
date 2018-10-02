77b34ccea55bb5acf0cafac5faba7127135bd4a9
Eric <ericmuita25@gmail.com>
2018-10-02 06:45:14 +0300

    eg of verbatim string

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nullable<int> x = null;
+            //Nullable<int> y = 5;
+            //Console.WriteLine(x < y);
+            //Console.ReadKey();
+            int x, y;
+            if ( x== null)
+            {
+                 y = 1;
+            }
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myname = @"My favourite book is entitled
+""Caucasian Chalk Circle""
+by Bertolt Brecht";
+            Console.WriteLine(myname);
+            Console.ReadKey();
+        }
+    }
+}
