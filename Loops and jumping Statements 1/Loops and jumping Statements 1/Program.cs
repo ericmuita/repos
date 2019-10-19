0fb7a1fd239dee86c98ea49d6c26f2a4939c1e8b
Eric <ericmuita25@gmail.com>
2019-10-19 22:22:16 +0300

    Loops and Jumping Statements updated

---
+++ Loops and jumping Statements 1/Loops and jumping Statements 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loops_and_jumping_Statements_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Loops$GoToStatements/Loops$GoToStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loops_GoToStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
